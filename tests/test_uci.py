03fa355aae1b40731f3a01d19b3ccb691d9fc3df
Yvonne Kaire <yvonne.e.kaire@gmail.com>
2023-06-21 21:27:27 -0700

    add test for entry with numbers in HCPS codes

--- tests/test_uci.py
+++ tests/test_uci.py
@@ -44,7 +44,20 @@ def test_gross_charge_entry(parser):
                 "Outside Lab Discount Cash Price": "N/A",
                 "FQHC FSC": "N/A",
                 "FQHC FSC Discount Cash Price": "N/A"
-            }
+            },
+            {
+                "Itemcode": "00010020_7809",
+                "Description": "HB BEVACIZUMAB 0.25 MG",
+                "CDM HCPCS": "C9257",
+                "UCI HB INPATIENT": "N/A",
+                "UCI HB INPATIENT Discount Cash Price": "N/A",
+                "UCI HB OUTPATIENT": "25.00",
+                "UCI HB OUTPATIENT Discount Cash Price": "10",
+                "Outside Lab": "N/A",
+                "Outside Lab Discount Cash Price": "N/A",
+                "FQHC FSC": "N/A",
+                "FQHC FSC Discount Cash Price": "N/A"
+            },
         ]
     }
 
@@ -65,7 +78,25 @@ def test_gross_charge_entry(parser):
             in_patient = False,
             payer = 'Cash',
             gross_charge = 212.8
-        ), 
+        ),
+        
+        ChargeMasterEntry(
+            procedure_identifier = '00010020_7809',
+            procedure_description = "HB BEVACIZUMAB 0.25 MG",
+            hcpcs_code = 'C9257',
+            in_patient = False,
+            payer = 'UCI HB',
+            gross_charge = 25.00
+        ),
+
+        ChargeMasterEntry(
+            procedure_identifier = '00010020_7809',
+            procedure_description = "HB BEVACIZUMAB 0.25 MG",
+            hcpcs_code = 'C9257',
+            in_patient = False,
+            payer = 'Cash',
+            gross_charge = 10.0
+        ),
     ]
 
     actual_results = list(parser.parse_artifacts({UCIChargeMasterParser.ARTIFACT_URL: io.BytesIO(json.dumps(row).encode('utf-8'))}))
