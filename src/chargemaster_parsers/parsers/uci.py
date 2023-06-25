f7d097c1b35c990dc2eb995543ccc6de8e797f09
Yvonne Kaire <yvonne.e.kaire@gmail.com>
2023-06-24 17:44:18 -0700

    Remove strip() from hcpcs code parser.

--- src/chargemaster_parsers/parsers/uci.py
+++ src/chargemaster_parsers/parsers/uci.py
@@ -22,21 +22,20 @@ class UCIChargeMasterParser(ChargeMasterParser):
                             hcpcs_code = entry.get("CDM HCPCS", None)
                             uci_hb_full_price = entry.get("UCI HB OUTPATIENT", None)
                             cash_price = entry.get("UCI HB OUTPATIENT Discount Cash Price", None)
-
                             if uci_hb_full_price != 'N/A': # add UCI HB payer entry only if there's a price listed
                                 yield ChargeMasterEntry(
                                     procedure_identifier = procedure_identifier,
                                     procedure_description = procedure_description,
-                                    hcpcs_code = hcpcs_code.strip(),
+                                    hcpcs_code = hcpcs_code,
                                     in_patient = False,
                                     payer = 'UCI HB',
                                     gross_charge = float(uci_hb_full_price.replace(',', '')))
                                 
                             if cash_price != 'N/A': # add cash payer entry only if there's a price listed
                                 yield ChargeMasterEntry(
-                                    procedure_identifier = procedure_identifier,
+                                    procedure_identifier = procedure_identifier.strip(),
                                     procedure_description = procedure_description,
-                                    hcpcs_code = hcpcs_code.strip(),
+                                    hcpcs_code = hcpcs_code,
                                     in_patient = False,
                                     payer = 'Cash',
                                     gross_charge = float(cash_price.replace(',', '')))
