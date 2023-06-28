579de43856a2be5fdf67160ad56d6c084ec3d32f
Yvonne Kaire <yvonne.e.kaire@gmail.com>
2023-06-27 22:50:14 -0700

    add misc info updates to uci parser

--- tests/test_uci.py
+++ tests/test_uci.py
@@ -18,6 +18,11 @@ def test_institution_name(parser):
 
 def test_gross_charge_entry(parser):
     row = {
+        "File Summary":[
+          {
+             "Prices Posted And Effective":"8/1/2022 12:00:00 AM"
+          }
+       ],
         "Gross Charges": [
             {
                 "Itemcode": "00000001_7143",
@@ -28,19 +33,6 @@ def test_gross_charge_entry(parser):
                 "UCI HB INPATIENT Discount Cash Price": "N/A",
                 "UCI HB OUTPATIENT": "532.00",
                 "UCI HB OUTPATIENT Discount Cash Price": "212.8",
-                "Outside Lab": "N/A",
-                "Outside Lab Discount Cash Price": "N/A",
-                "FQHC FSC": "N/A",
-                "FQHC FSC Discount Cash Price": "N/A",
-            },
-            {
-                "Itemcode": "99800003_7904",
-                "Description": "HB SKIN TEST READING (UCI ONLY)",
-                "CDM Revenue Code": "998",
-                "UCI HB INPATIENT": "N/A",
-                "UCI HB INPATIENT Discount Cash Price": "N/A",
-                "UCI HB OUTPATIENT": "N/A",
-                "UCI HB OUTPATIENT Discount Cash Price": "N/A",
                 "Outside Lab": "N/A",
                 "Outside Lab Discount Cash Price": "N/A",
                 "FQHC FSC": "N/A",
@@ -59,51 +51,63 @@ def test_gross_charge_entry(parser):
                 "FQHC FSC": "N/A",
                 "FQHC FSC Discount Cash Price": "N/A",
             },
+            {
+                "Itemcode": "99800003_7904",
+                "Description": "HB SKIN TEST READING (UCI ONLY)",
+                "CDM Revenue Code": "998",
+                "UCI HB INPATIENT": "N/A",
+                "UCI HB INPATIENT Discount Cash Price": "N/A",
+                "UCI HB OUTPATIENT": "N/A",
+                "UCI HB OUTPATIENT Discount Cash Price": "N/A",
+                "Outside Lab": "N/A",
+                "Outside Lab Discount Cash Price": "N/A",
+                "FQHC FSC": "N/A",
+                "FQHC FSC Discount Cash Price": "N/A",
+            },
+            
         ]
     }
 
     expected_result = [
         ChargeMasterEntry(
-            procedure_identifier="00000001_7143",
-            procedure_description="HB FINE NEEDLE ASPIRATION BX W/O IMG GDN 1ST LESION",
-            hcpcs_code="10021",
-            in_patient=False,
-            payer="UCI HB",
-            gross_charge=532.00,
+            procedure_description = "HB FINE NEEDLE ASPIRATION BX W/O IMG GDN 1ST LESION",
+            hcpcs_code = '10021',
+            in_patient = False,
+            payer = 'UCI HB',
+            gross_charge = 532.00,
+            extra_data = {"Itemcode": "00000001_7143"},
+            nubc_revenue_code = '761'   
+        ), 
+    
+        ChargeMasterEntry(
+            procedure_description = "HB FINE NEEDLE ASPIRATION BX W/O IMG GDN 1ST LESION",
+            hcpcs_code = '10021',
+            in_patient = False,
+            payer = 'Cash',
+            gross_charge = 212.8,
+            extra_data = {"Itemcode": "00000001_7143"},
+            nubc_revenue_code = '761',
         ),
         ChargeMasterEntry(
-            procedure_identifier="00000001_7143",
-            procedure_description="HB FINE NEEDLE ASPIRATION BX W/O IMG GDN 1ST LESION",
-            hcpcs_code="10021",
-            in_patient=False,
-            payer="Cash",
-            gross_charge=212.8,
+            procedure_description = "HB BEVACIZUMAB 0.25 MG",
+            hcpcs_code = 'C9257',
+            in_patient = False,
+            payer = 'UCI HB',
+            gross_charge = 25.00,
+            extra_data = {"Itemcode": "00010020_7809"},
+            nubc_revenue_code = None,
         ),
         ChargeMasterEntry(
-            procedure_identifier="00010020_7809",
-            procedure_description="HB BEVACIZUMAB 0.25 MG",
-            hcpcs_code="C9257",
-            in_patient=False,
-            payer="UCI HB",
-            gross_charge=25.00,
-        ),
-        ChargeMasterEntry(
-            procedure_identifier="00010020_7809",
-            procedure_description="HB BEVACIZUMAB 0.25 MG",
-            hcpcs_code="C9257",
-            in_patient=False,
-            payer="Cash",
-            gross_charge=10.0,
+            procedure_description = "HB BEVACIZUMAB 0.25 MG",
+            hcpcs_code = 'C9257',
+            in_patient = False,
+            payer = 'Cash',
+            gross_charge = 10.0,
+            extra_data = {"Itemcode": "00010020_7809"},
+            nubc_revenue_code = None,
         ),
     ]
 
-    actual_results = list(
-        parser.parse_artifacts(
-            {
-                UCIChargeMasterParser.ARTIFACT_URL: io.BytesIO(
-                    json.dumps(row).encode("utf-8")
-                )
-            }
-        )
-    )
-    assert sorted(expected_result) == sorted(actual_results)
+    actual_results = list(parser.parse_artifacts({UCIChargeMasterParser.ARTIFACT_URL: io.BytesIO(json.dumps(row).encode('utf-8'))}))
+    assert((expected_result) == (actual_results))
+        
