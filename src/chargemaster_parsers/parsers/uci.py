f423875f99e63d7090e1b7a6fdf693ac42e7ee61
Yvonne Kaire <6243394+princessbeane@users.noreply.github.com>
2023-09-03 20:43:37 -0700

    Uci fixes (#9)
    * add misc info
    updates to uci parser
    
    * update test
    
    * readme update
    
    * update UCI parser to match new schema
    
    * Remove unused code
    
    * remove procedure identifier from extra_data field
    
    * fix formatting

--- src/chargemaster_parsers/parsers/uci.py
+++ src/chargemaster_parsers/parsers/uci.py
@@ -8,42 +8,57 @@ class UCIChargeMasterParser(ChargeMasterParser):
     ARTIFACT_URLS = (ARTIFACT_URL,)
 
     def parse_artifacts(self, artifacts):
-        for artifact_url, artifact in artifacts.items():
-            data_dict = json.load(artifact)
+            for artifact_url, artifact in artifacts.items():
+                data_dict = json.load(artifact)
+                prev_hcpcs = None
 
-            for category, entries in data_dict.items():
-                # Currently only parsing the 'Gross Charges' entries. This could be extended to parse the
-                # other cateogories, such as 'Outpatient De-identified Negotiated Charge', but would require
-                # some more research.
-                if category == "Gross Charges":
-                    for entry in entries:
-                        procedure_identifier = entry.get("Itemcode", None)
-                        procedure_description = entry.get("Description", None)
-                        hcpcs_code = entry.get("CDM HCPCS", None)
-                        uci_hb_full_price = entry.get("UCI HB OUTPATIENT", None)
-                        cash_price = entry.get(
-                            "UCI HB OUTPATIENT Discount Cash Price", None
-                        )
-                        if (
-                            uci_hb_full_price != "N/A"
-                        ):  # add UCI HB payer entry only if there's a price listed
-                            yield ChargeMasterEntry(
-                                procedure_identifier=procedure_identifier,
-                                procedure_description=procedure_description,
-                                hcpcs_code=hcpcs_code,
-                                in_patient=False,
-                                payer="UCI HB",
-                                gross_charge=float(uci_hb_full_price.replace(",", "")),
-                            )
+                for category, entries in data_dict.items():
+                    if category == 'File Summary':
+                        effective_date = entries[0]["Prices Posted And Effective"]
+                        # "Hospital Name": "University of California Irvine Medical Center",
+                        # "Prices Posted And Effective": "8/1/2022 12:00:00 AM",
+                        # "File Disclaimer": "The information contained in this file is intended for informational purposes only and does not represent any obligation or agreement.",
+                        # "Payer Disclaimer": "In the absence of payment rates by plan type (HMO vs PPO), unless otherwise noted, please assume all plans are contracted under the same payer specific negotiated charge.",
+                        # "Gross Charge": "This section presents the standard gross charge for items And services.",
+                        # "Discounted Cash Price": "This section presents information regarding discounted cash pricing for those patients who decide to pay without insurance coverage.",
+                        # "Inpatient De-identified Negotiated Charge": "This section presents the de-identified minimum and maximum charge for items, services, and service packages that occur in the inpatient setting.",
+                        # "Inpatient Payer Specific Charge": "This section presents the payer specific negotiated charge for items, services, and service packages that occur in the inpatient setting.",
+                        # "Outpatient De-identified Negotiated Charge": "This section presents the de-identified minimum and maximum charge for items, services, and service packages that occur in the outpatient setting.",
+                        # "Outpatient Payer Specific Charge": "This section presents the payer specific negotiated charge for items, services, and service packages that occur in the outpatient setting."
+    
+                    # Currently only parsing the 'Gross Charges' entries. This could be extended to parse the 
+                    # other cateogories, such as 'Outpatient De-identified Negotiated Charge', but would require 
+                    # some more research.
+                    if category == 'Gross Charges':
+                        for entry in entries:
+                            procedure_identifier = entry.get("Itemcode", None) 
+                            procedure_description = entry.get("Description", None)
+                            hcpcs_code = entry.get("CDM HCPCS", None)
+                            uci_hb_full_price = entry.get("UCI HB OUTPATIENT RATE Price", None)
+                            cash_price = entry.get("UCI HB OUTPATIENT RATE Discounted Cash Price", None)
+                            nubc_revenue_code = entry.get("CDM Revenue Code", None)
 
-                        if (
-                            cash_price != "N/A"
-                        ):  # add cash payer entry only if there's a price listed
-                            yield ChargeMasterEntry(
-                                procedure_identifier=procedure_identifier,
-                                procedure_description=procedure_description,
-                                hcpcs_code=hcpcs_code,
-                                in_patient=False,
-                                payer="Cash",
-                                gross_charge=float(cash_price.replace(",", "")),
-                            )
+                            if uci_hb_full_price != 'N/A': # add UCI HB payer entry only if there's a price listed
+                                if uci_hb_full_price is not None:
+                                    yield ChargeMasterEntry(
+                                        procedure_identifier = procedure_identifier,
+                                        procedure_description = procedure_description,
+                                        hcpcs_code = hcpcs_code,
+                                        in_patient = False,
+                                        payer = 'UCI HB',
+                                        gross_charge = float(uci_hb_full_price.replace(',', '')),
+                                        nubc_revenue_code = nubc_revenue_code,
+                                    )
+
+                            if cash_price != 'N/A': # add cash payer entry only if there's a price listed
+                                if cash_price is not None:
+                                    yield ChargeMasterEntry(
+                                        procedure_identifier = procedure_identifier,
+                                        procedure_description = procedure_description,
+                                        hcpcs_code = hcpcs_code,
+                                        in_patient = False,
+                                        payer = 'Cash',
+                                        gross_charge = float(cash_price.replace(',', '')),
+                                        nubc_revenue_code = nubc_revenue_code,
+                                    )
+            
--- tests/test_uci.py
+++ tests/test_uci.py
@@ -18,6 +18,11 @@ def test_institution_name(parser):
 
 def test_gross_charge_entry(parser):
     row = {
+        "File Summary":[
+          {
+             "Prices Posted And Effective":"8/1/2022 12:00:00 AM"
+          }
+       ],
         "Gross Charges": [
             {
                 "Itemcode": "00000001_7143",
@@ -26,21 +31,8 @@ def test_gross_charge_entry(parser):
                 "CDM HCPCS": "10021",
                 "UCI HB INPATIENT": "N/A",
                 "UCI HB INPATIENT Discount Cash Price": "N/A",
-                "UCI HB OUTPATIENT": "532.00",
-                "UCI HB OUTPATIENT Discount Cash Price": "212.8",
-                "Outside Lab": "N/A",
-                "Outside Lab Discount Cash Price": "N/A",
-                "FQHC FSC": "N/A",
-                "FQHC FSC Discount Cash Price": "N/A",
-            },
-            {
-                "Itemcode": "99800003_7904",
-                "Description": "HB SKIN TEST READING (UCI ONLY)",
-                "CDM Revenue Code": "998",
-                "UCI HB INPATIENT": "N/A",
-                "UCI HB INPATIENT Discount Cash Price": "N/A",
-                "UCI HB OUTPATIENT": "N/A",
-                "UCI HB OUTPATIENT Discount Cash Price": "N/A",
+                "UCI HB OUTPATIENT RATE Price": "532.00",
+                "UCI HB OUTPATIENT RATE Discounted Cash Price": "212.8",
                 "Outside Lab": "N/A",
                 "Outside Lab Discount Cash Price": "N/A",
                 "FQHC FSC": "N/A",
@@ -51,59 +43,71 @@ def test_gross_charge_entry(parser):
                 "Description": "HB BEVACIZUMAB 0.25 MG",
                 "CDM HCPCS": "C9257",
                 "UCI HB INPATIENT": "N/A",
-                "UCI HB INPATIENT Discount Cash Price": "N/A",
-                "UCI HB OUTPATIENT": "25.00",
-                "UCI HB OUTPATIENT Discount Cash Price": "10",
+                "UCI HB INPATIENT Discounted Cash Price": "N/A",
+                "UCI HB OUTPATIENT RATE Price": "25.00",
+                "UCI HB OUTPATIENT RATE Discounted Cash Price": "10",
                 "Outside Lab": "N/A",
                 "Outside Lab Discount Cash Price": "N/A",
                 "FQHC FSC": "N/A",
                 "FQHC FSC Discount Cash Price": "N/A",
             },
+            {
+                "Itemcode": "99800003_7904",
+                "Description": "HB SKIN TEST READING (UCI ONLY)",
+                "CDM Revenue Code": "998",
+                "UCI HB INPATIENT": "N/A",
+                "UCI HB INPATIENT Discounted Cash Price": "N/A",
+                "UCI HB OUTPATIENT RATE Price": "N/A",
+                "UCI HB OUTPATIENT RATE Discounted Cash Price": "N/A",
+                "Outside Lab": "N/A",
+                "Outside Lab Discount Cash Price": "N/A",
+                "FQHC FSC": "N/A",
+                "FQHC FSC Discount Cash Price": "N/A",
+            },
+            
         ]
     }
 
     expected_result = [
         ChargeMasterEntry(
-            procedure_identifier="00000001_7143",
-            procedure_description="HB FINE NEEDLE ASPIRATION BX W/O IMG GDN 1ST LESION",
-            hcpcs_code="10021",
-            in_patient=False,
-            payer="UCI HB",
-            gross_charge=532.00,
+            procedure_identifier = "00000001_7143",       
+            procedure_description = "HB FINE NEEDLE ASPIRATION BX W/O IMG GDN 1ST LESION",
+            hcpcs_code = '10021',
+            in_patient = False,
+            payer = 'UCI HB',
+            gross_charge = 532.00,
+            nubc_revenue_code = '761'         
+        ), 
+    
+        ChargeMasterEntry(
+            procedure_identifier = "00000001_7143",       
+            procedure_description = "HB FINE NEEDLE ASPIRATION BX W/O IMG GDN 1ST LESION",
+            hcpcs_code = '10021',
+            in_patient = False,
+            payer = 'Cash',
+            gross_charge = 212.8,
+            nubc_revenue_code = '761',     
         ),
         ChargeMasterEntry(
-            procedure_identifier="00000001_7143",
-            procedure_description="HB FINE NEEDLE ASPIRATION BX W/O IMG GDN 1ST LESION",
-            hcpcs_code="10021",
-            in_patient=False,
-            payer="Cash",
-            gross_charge=212.8,
+            procedure_identifier = "00010020_7809",
+            procedure_description = "HB BEVACIZUMAB 0.25 MG",
+            hcpcs_code = 'C9257',
+            in_patient = False,
+            payer = 'UCI HB',
+            gross_charge = 25.00,
+            nubc_revenue_code = None,
         ),
         ChargeMasterEntry(
-            procedure_identifier="00010020_7809",
-            procedure_description="HB BEVACIZUMAB 0.25 MG",
-            hcpcs_code="C9257",
-            in_patient=False,
-            payer="UCI HB",
-            gross_charge=25.00,
-        ),
-        ChargeMasterEntry(
-            procedure_identifier="00010020_7809",
-            procedure_description="HB BEVACIZUMAB 0.25 MG",
-            hcpcs_code="C9257",
-            in_patient=False,
-            payer="Cash",
-            gross_charge=10.0,
+            procedure_identifier = "00010020_7809",
+            procedure_description = "HB BEVACIZUMAB 0.25 MG",
+            hcpcs_code = 'C9257',
+            in_patient = False,
+            payer = 'Cash',
+            gross_charge = 10.0,
+            nubc_revenue_code = None,
         ),
     ]
 
-    actual_results = list(
-        parser.parse_artifacts(
-            {
-                UCIChargeMasterParser.ARTIFACT_URL: io.BytesIO(
-                    json.dumps(row).encode("utf-8")
-                )
-            }
-        )
-    )
-    assert sorted(expected_result) == sorted(actual_results)
+    actual_results = list(parser.parse_artifacts({UCIChargeMasterParser.ARTIFACT_URL: io.BytesIO(json.dumps(row).encode('utf-8'))}))
+    assert((expected_result) == (actual_results))
+        
